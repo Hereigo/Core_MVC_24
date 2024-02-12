142f97b9f4f98b423498a1e41483b042ccfcff0a
ELEKS-SOFTWARE\andrii.plakhtii <andrii.plakhtii@eleks.com>
2024-02-12 17:31:42 +0200

    program update

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -6,13 +6,44 @@ string key_File_ = iGor.key_File_;
 
 try
 {
-    Cryptonic.GenerateAESKey(key_File_);
-    Cryptonic.Encrypt(decrypted, encrypted, key_File_);
-    Cryptonic.Decrypt(encrypted, decrypted, key_File_);
+    if (!File.Exists(key_File_))
+    {
+        Console.WriteLine("Key is generating...");
+        Cryptonic.GenerateAESKey(key_File_);
+        Console.WriteLine("Key has generated.");
+    }
+    else if (File.Exists(decrypted)) // Encrypting
+    {
+        if (File.Exists(encrypted))
+        {
+            Console.WriteLine("Decrypted BackUping...");
+            File.Move(encrypted, encrypted + "." + DateTime.Now.ToString("MMddHHmm"));
+            Console.WriteLine("Decrypted BackUped.");
+        }
+        else
+        {
+            Console.WriteLine("Encrypting...");
+            Cryptonic.Encrypt(decrypted, encrypted, key_File_);
+            Console.WriteLine("Encrypted finished.");
+
+            // TODO:
+            // should also remove Decrypted after test.
+        }
+    }
+    else if (File.Exists(encrypted)) // Decrypting
+    {
+        Console.WriteLine("Decrypting...");
+        Cryptonic.Decrypt(encrypted, decrypted, key_File_);
+        Console.WriteLine("Decrypted finished.");
+    }
+    else
+    {
+        Console.WriteLine("Nothing to do...");
+    }
 }
 catch (Exception ex)
 {
     Console.WriteLine("Error: " + ex.Message);
 }
 
-Console.WriteLine("Done.");
+Console.WriteLine("\r\n Finished.");
