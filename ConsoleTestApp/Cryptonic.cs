90feb81003dc4d009429117c5b98970002e9c100
ELEKS-SOFTWARE\andrii.plakhtii <andrii.plakhtii@eleks.com>
2024-02-12 16:07:55 +0200

    refactoring

--- ConsoleTestApp/Cryptonic.cs
+++ ConsoleTestApp/Cryptonic.cs
@@ -17,51 +17,36 @@ internal static class Cryptonic
 
     internal static void Encrypt(string inputFile, string encrypted, string key_File_)
     {
-        using (FileStream fsInput = new FileStream(inputFile, FileMode.Open))
-        {
-            using (FileStream fsOutput = new FileStream(encrypted, FileMode.Create))
-            {
-                using (AesManaged aes = new AesManaged())
-                {
-                    aes.KeySize = 256;
-                    aes.GenerateKey();
-                    aes.GenerateIV();
-                    // Initialization Vector (IV) used for encryption is prepended to the encrypted file.
+        using FileStream fsInput = new FileStream(inputFile, FileMode.Open);
+        using FileStream fsOutput = new FileStream(encrypted, FileMode.Create);
+        using AesManaged aes = new AesManaged();
 
-                    File.WriteAllBytes(key_File_, aes.Key);
-                    File.WriteAllBytes(key_File_ + ".iv", aes.IV);
+        aes.KeySize = 256;
+        aes.GenerateKey();
+        aes.GenerateIV();
+        // Initialization Vector (IV) used for encryption is prepended to the encrypted file.
 
-                    ICryptoTransform encryptor = aes.CreateEncryptor();
+        File.WriteAllBytes(key_File_, aes.Key);
+        File.WriteAllBytes(key_File_ + ".iv", aes.IV);
 
-                    using (CryptoStream cs = new CryptoStream(fsOutput, encryptor, CryptoStreamMode.Write))
-                    {
-                        fsInput.CopyTo(cs);
-                    }
-                }
-            }
-        }
+        ICryptoTransform encryptor = aes.CreateEncryptor();
+
+        using CryptoStream cs = new CryptoStream(fsOutput, encryptor, CryptoStreamMode.Write);
+        fsInput.CopyTo(cs);
     }
 
     internal static void Decrypt(string encrypted, string decrypted, string key_File_)
     {
-        using (FileStream fsInput = new FileStream(encrypted, FileMode.Open))
-        {
-            using (FileStream fsOutput = new FileStream(decrypted, FileMode.Create))
-            {
-                using (AesManaged aes = new AesManaged())
-                {
-                    aes.Key = File.ReadAllBytes(key_File_);
-                    aes.IV = File.ReadAllBytes(key_File_ + ".iv");
+        using FileStream fsInput = new FileStream(encrypted, FileMode.Open);
+        using FileStream fsOutput = new FileStream(decrypted, FileMode.Create);
+        using AesManaged aes = new AesManaged();
 
-                    ICryptoTransform decryptor = aes.CreateDecryptor();
+        aes.Key = File.ReadAllBytes(key_File_);
+        aes.IV = File.ReadAllBytes(key_File_ + ".iv");
 
-                    using (CryptoStream cs = new CryptoStream(fsOutput, decryptor, CryptoStreamMode.Write))
-                    {
-                        fsInput.CopyTo(cs);
-                    }
-                }
-            }
-        }
+        ICryptoTransform decryptor = aes.CreateDecryptor();
+
+        using CryptoStream cs = new CryptoStream(fsOutput, decryptor, CryptoStreamMode.Write);
+        fsInput.CopyTo(cs);
     }
-
 }
