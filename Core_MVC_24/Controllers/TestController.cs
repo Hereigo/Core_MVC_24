637df419274e7ea8779be0362987aca55b563acc
ELEKS-SOFTWARE\andrii.plakhtii <andrii.plakhtii@eleks.com>
2024-02-20 20:45:46 +0200

    test ctrl

---
+++ Core_MVC_24/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleTestApp;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_MVC_24.Controllers
+{
+    public class TestController(IWebHostEnvironment webHostEnvironment) : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment = webHostEnvironment;
+
+        public IActionResult Index()
+        {
+            string webRootPath = _webHostEnvironment.WebRootPath;
+
+            string filePath = Path.Combine(webRootPath, iGor.TestFile);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                return View();
+            }
+
+            return NotFound();
+        }
+    }
+}
