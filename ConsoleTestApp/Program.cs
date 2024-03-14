3a6948d703ea0263073282a0fc3022caf9f160c4
ELEKS-SOFTWARE\andrii.plakhtii <andrii.plakhtii@eleks.com>
2024-03-14 19:03:25 +0200

    refactoring

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System.IO.Compression;
-
 string _generalPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads\\EOS\\Tests\\");
 string _dabaseFile = Path.Combine(_generalPath, "workFolder\\TestDb24.dat");
 string _lockedFile = Path.Combine(_generalPath, "Tests.aaa");
 string _packedFile = Path.Combine(_generalPath, "Tests.pax");
 string _workFolder = Path.Combine(_generalPath, "workFolder\\");
 string blablatest;
-
 try
 {
     if (isDbFileExists())
@@ -15,29 +13,23 @@ try
         {
             string lockedFileBkp = $"{_lockedFile}.B4.{DateTime.Now:ddHHmmss}";
 
-            if (File.Exists(_lockedFile)) // Create Bkp
-            {
+            if (File.Exists(_lockedFile))
                 File.Copy(_lockedFile, lockedFileBkp, true);
-            }
 
-            if (File.Exists(lockedFileBkp)) // Pax
+            if (File.Exists(lockedFileBkp))
             {
                 File.Delete(_lockedFile);
-
                 ZipFile.CreateFromDirectory(_workFolder, _packedFile, CompressionLevel.SmallestSize, true);
             }
 
-            if (File.Exists(_packedFile)) // Cryps
-            {
+            if (File.Exists(_packedFile))
                 Cryptonic.EncryptByPass(_packedFile, _lockedFile, blablatest);
-            }
 
-            if (File.Exists(_lockedFile)) // Clear
+            if (File.Exists(_lockedFile))
             {
                 Directory.Delete(_workFolder, true);
                 File.Delete(_packedFile);
             }
-
             Console.WriteLine("Successfully Finished.");
         }
     }
@@ -48,19 +40,14 @@ try
             Cryptonic.DecryptByPass(_lockedFile, _packedFile, blablatest);
 
             if (File.Exists(_packedFile))
-            {
                 ZipFile.ExtractToDirectory(_packedFile, _generalPath);
-            }
 
             if (isDbFileExists())
-            {
                 File.Delete(_packedFile);
-            }
 
             Console.WriteLine("Successfully Prepaired For Work.");
         }
     }
-
     Console.WriteLine("\r\n Finished.");
 }
 catch (Exception ex)
@@ -79,10 +66,8 @@ bool isDbFileExists()
 bool SkipAction(bool isStart)
 {
     bool isWrong;
-
     Console.WriteLine(isStart ? "Write To Start Work?" : "Write To Finish?");
     blablatest = Console.ReadLine();
-
     if (isStart)
     {
         isWrong = string.IsNullOrEmpty(blablatest) || blablatest.Length < 12;
@@ -94,9 +79,7 @@ bool SkipAction(bool isStart)
     }
 
     if (isWrong)
-    {
         Console.WriteLine("\r\nWRONG Abrakadabra!");
-    }
 
     return isWrong;
 }
