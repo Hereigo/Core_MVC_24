65385139948c96e0a986391ad6de43f379f34436
ELEKS-SOFTWARE\andrii.plakhtii <andrii.plakhtii@eleks.com>
2024-05-29 15:54:44 +0300

    add md5

---
+++ ConsoleTestApp/HashCompute.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+
+namespace ConsoleTestApp
+{
+    internal class HashCompute
+    {
+        internal string FileHashCompute(string fileFullPath)
+        {
+            string hashString = string.Empty;
+
+            if (File.Exists(fileFullPath))
+            {
+                using (var md5 = MD5.Create())
+                {
+                    using (var stream = File.OpenRead(fileFullPath))
+                    {
+                        byte[] hashBytes = md5.ComputeHash(stream);
+
+                        hashString = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+                    }
+                }
+            }
+            return hashString;
+        }
+    }
+}
