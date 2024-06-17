770f25d61a072c68438526a32886128dcdedafa0
ELEKS-SOFTWARE\andrii.plakhtii <andrii.plakhtii@eleks.com>
2024-06-17 21:03:47 +0300

    small upd

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleTestApp;
+﻿using System.Xml.Serialization;
+using ConsoleTestApp;
 using FolkerKinzel.VCards;
 
 // =================================================================================
@@ -21,11 +22,20 @@ using var textReader = new StreamReader(fileVcfPath);
 using var vcfReader = new VcfReader(textReader);
 
 IEnumerable<VCard> result = vcfReader.ReadToEnd();
-var TEST = result.FirstOrDefault();
+
+//var TEST = result.FirstOrDefault();
+
+//var aaa = TEST.AnniversaryViews;
+
+//var TestProperties = TEST.GetType().GetProperties();
+
+//string TEST2 = System.Text.Json.JsonSerializer.Serialize(TestProperties);
 
 Console.WriteLine("The file \"{0}\" contains {1} vCards.",
                   Path.GetFileName(fileVcfPath),
                   result.Count());
+
+// https://4js.com/online_documentation/fjs-fgl-3.00.05-manual-html/c_fgl_odiagsqt_017.html
 
 Console.WriteLine("\r\nFinished.\r\n");
 Console.ReadLine();
