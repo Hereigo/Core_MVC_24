5b5f4c46c5d94c4cfc5cbfda85ab474a821dceba
ELEKS-SOFTWARE\andrii.plakhtii <andrii.plakhtii@eleks.com>
2024-06-23 18:27:43 +0300

    updace vcf read

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -1,7 +1,4 @@
 ﻿using ConsoleTestApp;
-using FolkerKinzel.VCards;
-using FolkerKinzel.VCards.Extensions;
-
 // =================================================================================
 // WORKING !!!!!!!!!!
 // =================================================================================
@@ -13,69 +10,9 @@ var main = new FileWorker();
 // TESTING :
 // =================================================================================
 
-string fileVcfPath = GIT_IGNORE.fileVcfPath;
 
-IList<VCard> vCards = Vcf.Load(fileVcfPath);
+VcfProfileParserFK.VcfParseRun();
 
-var vCard = vCards[0];
-
-string PrefferedMail = vCard.EMails.PrefOrNull()?.Value;
-
-string PrefferedPhone = vCard.Phones.PrefOrNull()?.Value;
-
-var TEST = true;
-
-//var TESTT2 = result.FirstOrDefault().Select(x => x.Addresses).Count(); //.FirstOrDefault();
-//var TESTT3 = result.FirstOrDefault().Select(x => x.AnniversaryViews).Count();
-//var TESTT4 = result.FirstOrDefault().Select(x => x.BirthDayViews).Count();
-//var TESTT5 = result.FirstOrDefault().Select(x => x.BirthPlaceViews).Count();
-//var TESTT6 = result.FirstOrDefault().Select(x => x.CalendarAccessUris).Count();
-//var TESTT7 = result.FirstOrDefault().Select(x => x.CalendarAddresses).Count();
-//var TESTT8 = result.FirstOrDefault().Select(x => x.CalendarUserAddresses).Count();
-//var TESTT9 = result.FirstOrDefault().Select(x => x.Categories).Count();
-//var TEST10 = result.FirstOrDefault().Select(x => x.DeathDateViews).Count();
-//var TEST11 = result.FirstOrDefault().Select(x => x.DeathPlaceViews).Count();
-//var TEST12 = result.FirstOrDefault().Select(x => x.DirectoryName).Count();
-//var TEST13 = result.FirstOrDefault().Select(x => x.DisplayNames).Count();
-//var TEST14 = result.FirstOrDefault().Select(x => x.EMails).Count();
-//var TEST15 = result.FirstOrDefault().Select(x => x.Entities).Count();
-//var TEST16 = result.FirstOrDefault().Select(x => x.Expertises).Count();
-//var TEST17 = result.FirstOrDefault().Select(x => x.FreeOrBusyUrls).Count();
-//var TEST18 = result.FirstOrDefault().Select(x => x.GenderViews).Count();
-//var TEST19 = result.FirstOrDefault().Select(x => x.GeoCoordinates).Count();
-//var TEST20 = result.FirstOrDefault().Select(x => x.GroupIDs).Count();
-//var TEST21 = result.FirstOrDefault().Select(x => x.Groups).Count();
-//var TEST22 = result.FirstOrDefault().Select(x => x.Hobbies).Count();
-//var TEST23 = result.FirstOrDefault().Select(x => x.ID).Count();
-//var TEST24 = result.FirstOrDefault().Select(x => x.Interests).Count();
-//var TEST25 = result.FirstOrDefault().Select(x => x.Keys).Count();
-//var TEST26 = result.FirstOrDefault().Select(x => x.Kind).Count();
-//var TEST27 = result.FirstOrDefault().Select(x => x.Languages).Count();
-//var TEST28 = result.FirstOrDefault().Select(x => x.Logos).Count();
-//var TEST29 = result.FirstOrDefault().Select(x => x.Mailer).Count();
-//var TEST30 = result.FirstOrDefault().Select(x => x.Members).Count();
-//var TEST31 = result.FirstOrDefault().Select(x => x.Messengers).Count();
-//var TEST32 = result.FirstOrDefault().Select(x => x.NameViews).Count();
-//var TEST33 = result.FirstOrDefault().Select(x => x.NickNames).Count();
-//var TEST34 = result.FirstOrDefault().Select(x => x.NonStandards).Count();
-//var TEST35 = result.FirstOrDefault().Select(x => x.Notes).Count();
-//var TEST36 = result.FirstOrDefault().Select(x => x.Organizations).Count();
-//var TEST37 = result.FirstOrDefault().Select(x => x.OrgDirectories).Count();
-//var TEST38 = result.FirstOrDefault().Select(x => x.Phones).Count();
-//var TEST39 = result.FirstOrDefault().Select(x => x.ProductID).Count();
-//var TEST40 = result.FirstOrDefault().Select(x => x.Profile).Count();
-//var TEST41 = result.FirstOrDefault().Select(x => x.Relations).Count();
-//var TEST42 = result.FirstOrDefault().Select(x => x.Roles).Count();
-//var TEST43 = result.FirstOrDefault().Select(x => x.Sounds).Count();
-//var TEST44 = result.FirstOrDefault().Select(x => x.Sources).Count();
-//var TEST45 = result.FirstOrDefault().Select(x => x.Sync).Count();
-//var TEST46 = result.FirstOrDefault().Select(x => x.TimeStamp).Count();
-//var TEST47 = result.FirstOrDefault().Select(x => x.TimeZones).Count();
-//var TEST48 = result.FirstOrDefault().Select(x => x.Titles).Count();
-//var TEST49 = result.FirstOrDefault().Select(x => x.Urls).Count();
-//var TEST50 = result.FirstOrDefault().Select(x => x.Version).Count();
-//var TEST51 = result.FirstOrDefault().Select(x => x.Xmls).Count();
-//var TEST52 = result.FirstOrDefault().Select(x => x.Access).Count();
 
 // https://4js.com/online_documentation/fjs-fgl-3.00.05-manual-html/c_fgl_odiagsqt_017.html
 
---
+++ ConsoleTestApp/VcfProfileParserFK.cs
@@ -0,0 +1,102 @@
+﻿using FolkerKinzel.VCards;
+using FolkerKinzel.VCards.Extensions;
+
+namespace ConsoleTestApp
+{
+    internal static class VcfProfileParserFK
+    {
+        public static void VcfParseRun()
+        {
+            string fileVcfPath = GIT_IGNORE.fileVcfPath;
+
+            IList<VCard> vCards = Vcf.Load(fileVcfPath);
+
+            var vCard = vCards[0];
+
+            System.Collections.ObjectModel.ReadOnlyCollection<string>? GivenName = vCard.NickNames.PrefOrNull()?.Value;
+
+            FolkerKinzel.VCards.Models.PropertyParts.Name? AdditionalName = vCard.NameViews.PrefOrNull()?.Value;
+
+            var TEST = new Profile();
+            //{
+            //    // ProfileID = vCard.AAAAA.PrefOrNull()?.Value,
+            //    AaaType = $"Export-{DateTime.Now:yyMMdd_HH}", // vCard.AAAAA.PrefOrNull()?.Value,
+            //    Name = vCard.DisplayNames.PrefOrNull()?.Value,
+                
+            //    GivenName = vCard.NickNames.PrefOrNull()?.Value,
+
+            //    AdditionalName = vCard.NameViews.PrefOrNull()?.Value,
+
+            //    FamilyName = vCard.DirectoryName.PrefOrNull()?.Value,
+            //    YomiName = vCard.DirectoryName.PrefOrNull()?.Value,
+
+            //    // GivenNameYomi = vCard.AAAAA.PrefOrNull()?.Value,
+            //    // AdditionalNameYomi = vCard.AAAAA.PrefOrNull()?.Value,
+            //    // FamilyNameYomi = vCard.AAAAA.PrefOrNull()?.Value,
+            //    // NamePrefix = vCard.AAAAA.PrefOrNull()?.Value,
+            //    // NameSuffix = vCard.AAAAA.PrefOrNull()?.Value,
+            //    // Initials = vCard.AAAAA.PrefOrNull()?.Value,
+            //    // Nickname = vCard.AAAAA.PrefOrNull()?.Value,
+            //    // ShortName = vCard.AAAAA.PrefOrNull()?.Value,
+            //    // MaidenName = vCard.AAAAA.PrefOrNull()?.Value,
+
+            //    Birthday = vCard.BirthDayViews.PrefOrNull()?.Value,
+            //    Gender = vCard.GenderViews.PrefOrNull()?.Value,
+            //    Location = vCard.GeoCoordinates.PrefOrNull()?.Value,
+
+            //    // BillingInformation = vCard.AAAAA.PrefOrNull()?.Value,
+            //    // DirectoryServer = vCard.AAAAA.PrefOrNull()?.Value,
+            //    // Mileage = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Occupation = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Hobby = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Sensitivity = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Priority = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Subject = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Notes = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Language = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Photo = vCard.AAAAA.PrefOrNull()?.Value,
+            //    GroupMembership = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Email1type = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Email1value = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Email2type = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Email2value = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Email3type = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Email3value = vCard.AAAAA.PrefOrNull()?.Value,
+            //    IM1type = vCard.AAAAA.PrefOrNull()?.Value,
+            //    IM1Service = vCard.AAAAA.PrefOrNull()?.Value,
+            //    IM1value = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Phone1type = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Phone1value = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Phone2type = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Phone2value = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Phone3type = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Phone3value = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Address1type = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Address1Formatted = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Address1Street = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Address1City = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Address1POBox = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Address1Region = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Address1PostalCode = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Address1Country = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Address1ExtendedAddress = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Org1type = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Org1Name = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Org1YomiName = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Org1Title = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Org1Department = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Org1Symbol = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Org1Location = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Org1JobDescription = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Website1type = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Website1value = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Website2type = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Website2value = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Event1type = vCard.AAAAA.PrefOrNull()?.Value,
+            //    Event1value = vCard.AAAAA.PrefOrNull()?.Value,
+            //    CustomField1type = vCard.AAAAA.PrefOrNull()?.Value,
+            //    CustomField1value = vCard.AAAAA.PrefOrNull()?.Value
+            //};
+        }
+    }
+}
